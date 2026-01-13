e47ba0b6ef51ba54d2f634d295b38e0398b1d2d8
Roman <jromanbrugno@gmail.com>
2026-01-12 21:42:23 -0300

    Feat: create ticket service project

---
+++ ticket-services/src/test/java/com/jrb/ticket_services/TicketServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrb.ticket_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
