3bab04c95e3eba2960e61a1dd1e6226610e0e56c
Roman <jromanbrugno@gmail.com>
2026-01-13 20:54:07 -0300

    Feat: Add more exception handlers

--- ticket-service/src/main/java/com/jrb/ticket_service/exception/ControllerAdvice.java
+++ ticket-service/src/main/java/com/jrb/ticket_service/exception/ControllerAdvice.java
@@ -2,8 +2,10 @@ package com.jrb.ticket_service.exception;
 
 import java.util.Date;
 
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +16,18 @@ public class ControllerAdvice {
     @ExceptionHandler(HallNotFound.class)
     public ResponseEntity<ErrorDTO> hallNotFoundHaldler(HallNotFound ex) {
         ErrorDTO response = new ErrorDTO(ex.getMessage(), new Date());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    public ResponseEntity<ErrorDTO> missingAttributes(InvalidDataAccessApiUsageException ex) {
+        ErrorDTO response = new ErrorDTO("Error body attributes", new Date());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDTO> missmatchType(HttpMessageNotReadableException ex) {
+        ErrorDTO response = new ErrorDTO("Error attributes type", new Date());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
