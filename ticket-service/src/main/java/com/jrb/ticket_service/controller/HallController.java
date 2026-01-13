a40de01d1bccdcde3ec018bf6a83f8c6062c7e40
Roman <jromanbrugno@gmail.com>
2026-01-13 20:45:23 -0300

    Feat: create delete hall endpoint

--- ticket-service/src/main/java/com/jrb/ticket_service/controller/HallController.java
+++ ticket-service/src/main/java/com/jrb/ticket_service/controller/HallController.java
@@ -2,12 +2,14 @@ package com.jrb.ticket_service.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jrb.ticket_service.dtos.CreateHallDTO;
+import com.jrb.ticket_service.dtos.DeleteHallRequestDTO;
 import com.jrb.ticket_service.service.HallService;
 
 import jakarta.validation.Valid;
@@ -25,5 +27,11 @@ public class HallController {
     public ResponseEntity<String> createHall(@Valid @RequestBody CreateHallDTO request) {
         hallService.createHall(request);
         return new ResponseEntity<>("Hall was created successfully", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<String> deleteHall(@RequestBody DeleteHallRequestDTO request) {
+        hallService.deleteHall(request);
+        return new ResponseEntity<>("Hall was deleted successfully", HttpStatus.ACCEPTED);
     }
 }
