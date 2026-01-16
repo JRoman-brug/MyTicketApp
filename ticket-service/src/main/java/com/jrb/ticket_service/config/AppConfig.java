8cfeff5a7c278892e73350da0d41a1a7013a1034
Roman <jromanbrugno@gmail.com>
2026-01-16 20:53:39 -0300

    Feat: enable cors in ticket-service

--- ticket-service/src/main/java/com/jrb/ticket_service/config/AppConfig.java
+++ ticket-service/src/main/java/com/jrb/ticket_service/config/AppConfig.java
@@ -1,5 +1,35 @@
 package com.jrb.ticket_service.config;
 
-public class AppConfig {
+import java.util.List;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // ¡OJO AQUÍ! Pon la URL exacta de tu frontend (Vite suele usar el puerto 5173)
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        // Métodos HTTP que vas a permitir
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // Cabeceras permitidas (Authorization es vital si usas JWT)
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        // Permitir credenciales (cookies o cabeceras de autenticación)
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        // Aplica esta configuración a TODAS las rutas de tu API
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- ticket-service/src/main/java/com/jrb/ticket_service/config/SecurityConfig.java
+++ ticket-service/src/main/java/com/jrb/ticket_service/config/SecurityConfig.java
@@ -5,13 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+    private CorsConfigurationSource corsConfigurationSource;
+
+    public SecurityConfig(CorsConfigurationSource corsConfigurationSource) {
+        this.corsConfigurationSource = corsConfigurationSource;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf.disable())
                 // To enabled h2 console
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
