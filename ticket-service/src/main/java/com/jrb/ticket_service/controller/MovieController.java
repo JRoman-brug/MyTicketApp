cdc4359d1ddc53d60493cc861c5d53ae773830f2
Roman <jromanbrugno@gmail.com>
2026-01-19 23:12:31 -0300

    Feat: create controller for movies

---
+++ ticket-service/src/main/java/com/jrb/ticket_service/controller/MovieController.java
@@ -0,0 +1,50 @@
+package com.jrb.ticket_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jrb.ticket_service.dtos.MovieDTOs;
+import com.jrb.ticket_service.service.MovieService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    private MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDTOs.Response> getMovie(@PathVariable Long id) {
+        MovieDTOs.Response response = movieService.getMovie(id);
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieDTOs.Response> createMovie(@RequestBody MovieDTOs.CreateRequest request) {
+        MovieDTOs.Response response = movieService.createMovie(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PatchMapping
+    public ResponseEntity<MovieDTOs.Response> updateMovie(@RequestBody MovieDTOs.UpdateRequest request) {
+        MovieDTOs.Response response = movieService.updateMovie(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build();
+    }
+}
