6c2098309fa2cb89285ca9f41174ccc804c0df2c
Roman <jromanbrugno@gmail.com>
2026-01-22 18:37:56 -0300

    Feat: add mapper to SeatService

--- ticket-service/src/main/java/com/jrb/ticket_service/service/SeatService.java
+++ ticket-service/src/main/java/com/jrb/ticket_service/service/SeatService.java
@@ -6,26 +6,29 @@ import com.jrb.ticket_service.dtos.SeatDTOs;
 import com.jrb.ticket_service.entity.Seat;
 import com.jrb.ticket_service.exception.ErrorCode;
 import com.jrb.ticket_service.exception.SeatNotFound;
+import com.jrb.ticket_service.mapper.SeatMapper;
 import com.jrb.ticket_service.repository.SeatRepository;
 
 @Service
 public class SeatService {
 
     private SeatRepository seatRepository;
+    private SeatMapper seatMapper;
 
-    public SeatService(SeatRepository seatRepository) {
+    public SeatService(SeatRepository seatRepository, SeatMapper seatMapper) {
         this.seatRepository = seatRepository;
+        this.seatMapper = seatMapper;
     }
 
     public SeatDTOs.Response reserveSeat(Long id) {
         Seat seat = seatRepository.findById(id).orElseThrow(() -> new SeatNotFound(ErrorCode.SEAT_NOT_FOUND));
         seatRepository.save(seat);
-        return new SeatDTOs.Response(seat.getId(), seat.getRow(), seat.getColumn(), seat.getLabel());
+        return seatMapper.toResponse(seat);
     }
 
     public SeatDTOs.Response releaseSeat(Long id) {
         Seat seat = seatRepository.findById(id).orElseThrow(() -> new SeatNotFound(ErrorCode.SEAT_NOT_FOUND));
         seatRepository.save(seat);
-        return new SeatDTOs.Response(seat.getId(), seat.getRow(), seat.getColumn(), seat.getLabel());
+        return seatMapper.toResponse(seat);
     }
 }
