4925ca2f716bcf9477c52e6c6879f8e811821cac
Roman <jromanbrugno@gmail.com>
2026-01-26 22:40:47 -0300

    Feat: add button to back to movies

--- Frontend/src/pages/(admin)/movies/createMovie/CreateMoviePage.tsx
+++ Frontend/src/pages/(admin)/movies/createMovie/CreateMoviePage.tsx
@@ -1,8 +1,21 @@
 import CreateMovieFrom from '@/features/movies/components/CreateMovieFrom';
+import { Button } from '@heroui/react';
+import { useNavigate } from 'react-router-dom';
 
 function CreateMoviePage() {
+  const navigate = useNavigate();
+  const onBack = () => {
+    navigate('/movies');
+  };
   return (
-    <main className="w-screen h-screen flex flex-col justify-center items-center gap-4">
+    <main className="w-screen h-screen flex flex-col justify-center items-center gap-4 relative">
+      <Button
+        color="primary"
+        onPress={onBack}
+        className="absolute top-5 left-5"
+      >
+        Back
+      </Button>
       <h1 className="font-bold text-white text-4xl">Create a movie</h1>
       <CreateMovieFrom />;
     </main>
