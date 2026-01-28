af64451c88522972af7fc64207ffb3f7b2705049
Roman <jromanbrugno@gmail.com>
2026-01-27 22:34:21 -0300

    Feat: add endpoint to retrieve paginated showtimes

--- ticket-service/src/main/java/com/jrb/ticket_service/controller/ShowtimeController.java
+++ ticket-service/src/main/java/com/jrb/ticket_service/controller/ShowtimeController.java
@@ -1,17 +1,22 @@
 package com.jrb.ticket_service.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jrb.ticket_service.dtos.PageResponse;
 import com.jrb.ticket_service.dtos.ShowtimeDTOs;
 import com.jrb.ticket_service.service.ShowtimeService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -27,6 +32,17 @@ public class ShowtimeController {
     @GetMapping("/{showtimeId}")
     public ResponseEntity<ShowtimeDTOs.Response> getShowtime(@PathVariable Long showtimeId) {
         ShowtimeDTOs.Response response = showtimeService.getShowtime(showtimeId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<ShowtimeDTOs.Response>> getAllShowtime(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            HttpServletRequest request) {
+        Page<ShowtimeDTOs.Response> allShowtimes = showtimeService.getAllShowtime(page, size);
+        String baseUrl = request.getRequestURL().toString();
+        PageResponse<ShowtimeDTOs.Response> response = new PageResponse<>(allShowtimes, baseUrl);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
