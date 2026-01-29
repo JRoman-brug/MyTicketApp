be5380d80fbc0952b0a8b797a907eaf62975f5e6
Roman <jromanbrugno@gmail.com>
2026-01-28 22:57:25 -0300

    Feat: update seatMapper to new seatDto response

--- ticket-service/src/main/java/com/jrb/ticket_service/mapper/SeatMapper.java
+++ ticket-service/src/main/java/com/jrb/ticket_service/mapper/SeatMapper.java
@@ -1,13 +1,15 @@
 package com.jrb.ticket_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.jrb.ticket_service.dtos.SeatDTOs;
 import com.jrb.ticket_service.entity.Seat;
 
 @Mapper(componentModel = "spring")
 public interface SeatMapper {
-    SeatDTOs.Response toResponse(Seat seat);
+    @Mapping(target = "isAvailable", source = "isAvailable")
+    SeatDTOs.Response toResponse(Seat seat, boolean isAvailable);
 
     SeatDTOs.Summary toSummary(Seat seat);
 }
