b03f3d0daad469273c83db5297f986f0eb0afc63
Roman <jromanbrugno@gmail.com>
2026-02-03 20:40:16 -0300

    Feat: implement a button to delete a showtime

--- Frontend/src/features/shotime/components/ShowtimeActions.tsx
+++ Frontend/src/features/shotime/components/ShowtimeActions.tsx
@@ -1,19 +1,22 @@
 import { Button } from '@heroui/react';
 import { Pencil, Trash2 } from 'lucide-react';
+import { useDeleteShowtime } from '../hooks/useDeleteMovie';
+import { useNavigate } from 'react-router-dom';
+import ConfirmModal from '@/components/ConfirmModal';
 
 interface ShowtimeActionsProps {
   readonly showtimeId: number;
 }
 function ShowtimeActions({ showtimeId }: ShowtimeActionsProps) {
+  const navigate = useNavigate();
+  const { deleteShowtime } = useDeleteShowtime();
+
+  const deleteHandler = () => {
+    deleteShowtime(showtimeId);
+    navigate('/showtimes');
+  };
   return (
     <div className="flex gap-2 justify-end">
-      <Button
-        aria-label={`Delete showtime with id ${showtimeId}`}
-        isIconOnly
-        color="danger"
-      >
-        <Trash2 />
-      </Button>
       <Button
         aria-label={`Edit showtime with id ${showtimeId}`}
         isIconOnly
@@ -21,6 +24,22 @@ function ShowtimeActions({ showtimeId }: ShowtimeActionsProps) {
       >
         <Pencil />
       </Button>
+      <ConfirmModal
+        title={`Delete showtime with id ${showtimeId}`}
+        bodyText={`Are you sure delete this showtime?`}
+        action={deleteHandler}
+      >
+        {(onOpen) => (
+          <Button
+            aria-label={`Delete showtime with id ${showtimeId}`}
+            isIconOnly
+            color="danger"
+            onPress={onOpen}
+          >
+            <Trash2 />
+          </Button>
+        )}
+      </ConfirmModal>
     </div>
   );
 }
