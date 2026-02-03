a61c941ffefd4384800ecbfb4c891461c4397570
Roman <jromanbrugno@gmail.com>
2026-02-03 00:04:35 -0300

    Feat: implement a service to update a showtime

--- Frontend/src/features/shotime/services/showtimeService.ts
+++ Frontend/src/features/shotime/services/showtimeService.ts
@@ -3,6 +3,7 @@ import { api } from '@/lib/axios';
 import type {
   ShowtimeDetailsType,
   CreateShowtimeType,
+  UpdateShowtimeType,
 } from '../types/showtimeType';
 
 export const showtimeService = {
@@ -20,6 +21,11 @@ export const showtimeService = {
 
   createShowtime: async (request: CreateShowtimeType) => {
     const { data } = await api.post(`/showtimes`, request);
+    return data;
+  },
+
+  updateShowtime: async (request: UpdateShowtimeType) => {
+    const { data } = await api.patch(`/showtimes`, request);
     return data;
   },
 };
