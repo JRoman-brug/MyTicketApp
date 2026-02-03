d72af6f65777f24ea1c3035509a5a0ed5553534e
Roman <jromanbrugno@gmail.com>
2026-02-03 00:01:14 -0300

    Feat: implement service to get one showtime by id

--- Frontend/src/features/shotime/services/showtimeService.ts
+++ Frontend/src/features/shotime/services/showtimeService.ts
@@ -5,7 +5,13 @@ import type { ShowtimeDetailsType } from '../types/showtimeType';
 export const showtimeService = {
   getAllShowtime: async () => {
     const { data } =
-      await api.get<PaginatedResponse<ShowtimeDetailsType>>(`/api/showtimes`);
+      await api.get<PaginatedResponse<ShowtimeDetailsType>>(`/showtimes`);
+    return data;
+  },
+  getShowtimeById: async (showtimeId: number) => {
+    const { data } = await api.get<ShowtimeDetailsType>(
+      `/showtimes/${showtimeId}`
+    );
     return data;
   },
 };
