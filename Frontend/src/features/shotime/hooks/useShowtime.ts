a033f7e944562fe35383bb942ca1b47331f9e1fa
Roman <jromanbrugno@gmail.com>
2026-02-03 00:36:32 -0300

    Feat: implement custom hook useShowtime to fetch showtime details

---
+++ Frontend/src/features/shotime/hooks/useShowtime.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import { CANT_RETRY_TIME, showtimesKeys, STALE_TIME } from './showTimeKeys';
+import { showtimeService } from '../services/showtimeService';
+
+export const useShowtime = (showtimeId: number) => {
+  const query = useQuery({
+    queryKey: showtimesKeys.detail(showtimeId),
+    queryFn: () => showtimeService.getShowtimeById(showtimeId),
+    staleTime: STALE_TIME,
+    retry: CANT_RETRY_TIME,
+  });
+  return {
+    movie: query.data,
+    isError: query.isError,
+    isLoading: query.isLoading,
+    error: query.error,
+  };
+};
