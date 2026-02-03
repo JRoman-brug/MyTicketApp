2ec2601b3dc9ee0e571b6f4f66be5e20e854c71f
Roman <jromanbrugno@gmail.com>
2026-02-02 23:58:17 -0300

    Refactor: rename request and response interfaces for consistency

--- Frontend/src/features/shotime/types/showtimeType.ts
+++ Frontend/src/features/shotime/types/showtimeType.ts
@@ -1,26 +1,26 @@
 import type { HallDetailsType } from '@/features/cinema/types';
 import type { MovieSummaryType } from '@/features/movies/types/movieType';
 
-export interface CreateRequest {
+export interface CreateRequestType {
   startTime: string;
   movieId: number;
   hallId: number;
 }
 
-export interface UpdateRequest {
+export interface UpdateRequestType {
   id: number;
   startTime: string;
   hallId: string;
 }
 
-export interface Response {
+export interface ShowtimeDetailsType {
   startTime: string;
   movie: MovieSummaryType;
   hall: HallDetailsType;
   totalSeats: number;
   availableSeats: number;
 }
-export interface Summary {
+export interface ShowtimeSummaryType {
   id: number;
   startTime: string;
   movieTitle: string;
