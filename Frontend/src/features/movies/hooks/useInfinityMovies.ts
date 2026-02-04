da145a7f07ede8e00272e47691e49c3dd22aa8cc
Roman <jromanbrugno@gmail.com>
2026-02-03 21:13:21 -0300

    Feat: implement useInfiniteMovies hook for fetching paginated movie data

---
+++ Frontend/src/features/movies/hooks/useInfinityMovies.ts
@@ -0,0 +1,15 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { movieService } from '../services/movieService';
+
+export const useInfiniteMovies = () => {
+  return useInfiniteQuery({
+    queryKey: ['movies-infinite'],
+    // pageParam es el número de página actual que gestiona React Query
+    queryFn: ({ pageParam = 0 }) => movieService.getAllMovies(pageParam, 10), // Tamaño 10
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      // Si no es la última página, la siguiente es current + 1
+      return lastPage.nextPath ? undefined : lastPage.pageNumber + 1;
+    },
+  });
+};
