3f2219c53cb1f082565dbd182546475f23151e86
Roman <jromanbrugno@gmail.com>
2026-02-05 20:25:02 -0300

    Feat: implement a service to get all hall from database

--- Frontend/src/features/cinema/services/cinemaService.ts
+++ Frontend/src/features/cinema/services/cinemaService.ts
@@ -1,11 +1,20 @@
 import { api } from '@/lib/axios';
-import type { CinemaHallType, SeatType } from '@/features/cinema/types';
+import type {
+  CinemaHallType,
+  HallDetailsType,
+  SeatType,
+} from '@/features/cinema/types';
 
 // Services only talk with the provider (this case is axios)
 // We define all routes that communicate with the backend
 export const cinemaService = {
   getHallById: async (hallId: string) => {
     const { data } = await api.get<CinemaHallType>(`/hall/${hallId}`);
+    return data;
+  },
+
+  getAllHalls: async () => {
+    const { data } = await api.get<HallDetailsType[]>(`/hall`);
     return data;
   },
   reserveSeatById: async (seatId: string) => {
